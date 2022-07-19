5cde7616b06f7d7b5a978d4d51829ad37e1c060a
Younus7447 <108931307+Younus7447@users.noreply.github.com>
2022-07-19 18:01:04 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+class ForLoop
+ {
+	public static void main(String[] args)
+	 {
+		System.out.println(" ");
+		for(int i=11; i>3; i--)
+		{
+			System.out.println("The value of i: " + i);
+		}
+
+	  }
+
+}
